de935af134f2a50420c95749e6cccb99fb926e16
Marcin Galiński <marcin.galinski95@gmail.com>
2016-01-18 00:40:02 +0100

    refactoring

--- connect_four.rb
+++ connect_four.rb
@@ -1,13 +1,15 @@
-class Field
-  attr_accessor :state
-end
+module ConnectFour
+  class Field
+    attr_accessor :state
+  end
 
-class Board
-  attr_accessor :fields
-end
+  class Board
+    attr_accessor :fields
+  end
 
-class Game
-  def initialize
-    @board = Board.new
+  class Game
+    def initialize
+      @board = Board.new
+    end
   end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,15 +1,16 @@
 require_relative '../connect_four'
+include ConnectFour
 
-describe Game do
-  it 'creates new board on initialize' do
-    expect(subject.instance_variable_get(:@board)).to be_an_instance_of(Board)
-  end
+describe Field do
+  it { is_expected.to respond_to(:state) }
 end
 
 describe Board do
   it { is_expected.to respond_to(:fields) }
 end
 
-describe Field do
-  it { is_expected.to respond_to(:state) }
+describe Game do
+  it 'creates new board on initialize' do
+    expect(subject.instance_variable_get(:@board)).to be_an_instance_of(Board)
+  end
 end
